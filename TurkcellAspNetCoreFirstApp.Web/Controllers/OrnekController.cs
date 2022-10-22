c7333727a99ee02b4244cb482f5f66a4e9502266
ramazanalperenakil <ramazanalperenakil@gmail.com>
2022-10-22 20:03:12 +0300

    Proje dosyası ekle.

---
+++ TurkcellAspNetCoreFirstApp.Web/Controllers/OrnekController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TurkcellAspNetCoreFirstApp.Web.Controllers
+{
+    public class OrnekController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.name = "ASP Net Core";
+            ViewBag.name = new List<string>() {"Ali","Hasan","Mehmet" };
+            ViewData["NameList"] = new List<string>() {"Ali","Hasan","Mehmet" };
+            ViewData["age"] = 22;
+
+
+            ViewBag.Person = (new { Id = 1, Name = "Kalem 1", Age = 100 });
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return RedirectToAction("Index", "Ornek");
+            /*return View()*/;
+        }
+        public IActionResult ParametreView(int id)
+        {
+            return RedirectToAction("jsonResultParametre", "Ornek" ,new { id = id });
+            /*return View()*/
+            ;
+        }
+
+        public IActionResult ContentResult()
+        {
+            return Content("ContentResult");
+        }
+
+        public IActionResult jsonResult()
+        {
+            return Json(new {Id=1, name="Kalem 1", price = 100});
+        }
+
+        public IActionResult jsonResultParametre(int id)
+        {
+            return Json(new { Id = id });
+        }
+
+        public IActionResult Empty()
+        {
+            return Empty();
+        }
+    }
+}
---
+++ TurkcellAspNetCoreFirstApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TurkcellAspNetCoreFirstApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
